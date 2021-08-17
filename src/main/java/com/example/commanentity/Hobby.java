db25eaa81a3646e1e274ea838a3ce8b9c3653f9b
anas1606 <darshan@eliteevince.in>
2021-08-17 10:15:05 +0530

    update

---
+++ src/main/java/com/example/commanentity/Hobby.java
@@ -0,0 +1,21 @@
+package com.example.commanentity;
+
+import com.example.commanentity.enums.Status;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Hobby extends AbstractDomain {
+    @Id
+    private String hobbyUID;
+    private String hobby;
+
+    public Hobby() {
+        this.hobbyUID = UUID.randomUUID().toString();
+        this.setStatus(Status.ACTIVE);
+    }
+}
