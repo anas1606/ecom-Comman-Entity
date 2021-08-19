964ffa468fde22f02db764d3ec14006041449ef9
anas1606 <darshan@eliteevince.in>
2021-08-19 10:44:04 +0530

    update

--- src/main/java/com/example/commanentity/Vendor.java
+++ src/main/java/com/example/commanentity/Vendor.java
@@ -19,6 +19,8 @@ public class Vendor extends AbstractDomain {
     private String emailid;
     private String password;
     private String phoneno;
+    private String email_verification_otp;
+    private boolean emailverified;
     private String profile_url;
     private String session_token;
 
