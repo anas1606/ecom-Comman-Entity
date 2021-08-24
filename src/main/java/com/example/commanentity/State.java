8f27e6949b220a4cd09fea7d0000944181814a3b
anas1606 <darshan@eliteevince.in>
2021-08-24 09:50:30 +0530

    update

--- src/main/java/com/example/commanentity/State.java
+++ src/main/java/com/example/commanentity/State.java
@@ -3,8 +3,7 @@ package com.example.commanentity;
 import com.example.commanentity.enums.Status;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -13,6 +12,10 @@ public class State extends AbstractDomain {
     @Id
     private String id;
     private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_country_id")
+    private Country country;
 
     public State() {
         this.id = UUID.randomUUID().toString();
